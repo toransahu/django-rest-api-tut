697168942c6b78e57c895c74b9f9ddd4e94757ff
TORAN <toran.sahu@yahoo.com>
2017-08-22 23:56:43 +0530

    implemented rest_framework.routers

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -13,9 +13,36 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
+from rest_framework import routers
+from tutorial.quickstart import views
+
+"""
+Routers is simple tool to speed-up implementing of API.
+Using Router will connect your ViewSet into "standarized"
+(it's not standard in any global way,
+just some structure that was implemented
+by creators of Django REST framework)
+structure of URLs.
+That way you don't have to create your urlpatterns by hand and
+you're guaranteed that all of your urls are consistent.
+"""
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+"""
+Because we're using viewsets instead of views, we can automatically generate the URL conf for our API, by simply registering the viewsets with a router class.
+
+Again, if we need more control over the API URLs we can simply drop down to using regular class-based views, and writing the URL conf explicitly.
+
+Finally, we're including default login and logout views for use with the browsable API. That's optional, but useful if your API requires authentication and you want to use the browsable API.
+"""
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
