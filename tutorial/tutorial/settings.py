a912c027ded8720eeda8a4af99b5c493db8417a6
TORAN <toran.sahu@yahoo.com>
2017-08-22 23:57:30 +0530

    added rest_framework to INSTALLED_APPS

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -37,7 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
+
+"""
+We'd like to turn on pagination,
+and we want our API to only be accessible to admin users.
+"""
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
