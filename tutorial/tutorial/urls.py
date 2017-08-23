c934d69e770244b6049a00c69ab8b482565d69e1
TORAN <toran.sahu@yahoo.com>
2017-08-23 23:59:42 +0530

    removed $ from r'^snippets$/'

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -45,5 +45,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^snippets/$',include('snippets.url')),
+    url(r'^snippets/',include('snippets.url')),
 ]
