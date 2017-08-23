3ba2921cf1cdb23cef64e386b201ec93c8fc1af7
TORAN <toran.sahu@yahoo.com>
2017-08-23 19:40:16 +0530

    updated comments

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.Serializer):
+    """
+    The first part of the serializer class defines the fields that get serialized/deserialized. The create() and update() methods define how fully fledged instances are created or modified when calling serializer.save()
+    A serializer class is very similar to a Django Form class, and includes similar validation flags on the various fields, such as required, max_length and default.
+    The field flags can also control how the serializer should be displayed in certain circumstances, such as when rendering to HTML. The {'base_template': 'textarea.html'} flag above is equivalent to using widget=widgets.Textarea on a Django Form class. This is particularly useful for controlling how the browsable API should be displayed, as we'll see later in the tutorial.
+    We can actually also save ourselves some time by using the ModelSerializer class, as we'll see later, but for now we'll keep our serializer definition explicit.
+    """
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
