a99957e20c6f7b644e0883b5b217faec500ec357
toransahu <toran.sahu@yahoo.com>
2017-08-28 13:37:31 +0530

    implemented view using 'generics.ListCreateAPIView' & 'generics.RetrieveUpdateDestroyAPIView'

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,54 +1,29 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
 
 # Create your views here.
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all snippets, or crete a new snippet.
 
-    The base class provides the core functionality,
-    and the mixin classes provide the .list() and .create() actions.
-    We're then explicitly binding the get
-    and post methods to the appropriate actions.
+    REST framework provides a set of already mixed-in generic views.
     """
     
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self,request,*args, **kwargs):
-        return self.list(request, *args, **kwargs)
-            
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request,*args, **kwargs)
     
-    
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a snippet instance.
 
-    Adding in mixins to provide the .retrieve(), .update() and .destroy() actions.
+    REST framework provides a set of already mixed-in generic views
     """
+    
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request,*args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-    
     
