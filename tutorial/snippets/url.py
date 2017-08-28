193367e1ef297dba83af58dd23c319e5cb7d4ea5
TORAN <toran.sahu@yahoo.com>
2017-08-28 09:45:14 +0530

    added format_suffix_patterns support to exesting url

--- tutorial/snippets/url.py
+++ tutorial/snippets/url.py
@@ -1,7 +1,16 @@
 from django.conf.urls import url
 from snippets import views
+"""
+to append a set of format_suffix_patterns in addition to the existing URLs.
+"""
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^$',views.snippet_list),
     url(r'^(?P<pk>[0-9]+)/$',views.snippet_detail),
 ]
+
+"""
+to append a set of format_suffix_patterns in addition to the existing URLs.
+"""
+urlpatterns = format_suffix_patterns(urlpatterns)
