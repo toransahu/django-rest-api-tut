b98d646ff4024fcb2cd7b3f0a3ae66209b5b7733
toransahu <toran.sahu@yahoo.com>
2017-08-28 16:06:30 +0530

    rectified typos

--- tutorial/snippets/url.py
+++ tutorial/snippets/url.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^$',views.SnippetList.as_view()),
     url(r'^(?P<pk>[0-9]+)/$',views.SnippetDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+/$', views.UserDetail.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
 """
