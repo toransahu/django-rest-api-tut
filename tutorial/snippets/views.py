652b7bd7c5443dfbcc7b0739dde17036b4d39efb
toransahu <toran.sahu@yahoo.com>
2017-08-28 15:32:06 +0530

    Implemented UserList and UserDetail generic class view

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,6 +2,8 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 
+from snippets.serializers import UserSerializer
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -26,4 +28,12 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(genericx.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     
