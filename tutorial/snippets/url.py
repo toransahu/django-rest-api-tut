46ad1fd452bb8f056ee2b4ba207bfd5685d48f6d
toransahu <toran.sahu@yahoo.com>
2017-08-31 16:57:43 +0530

    added url name to SnippetList & UserList

--- tutorial/snippets/url.py
+++ tutorial/snippets/url.py
@@ -6,11 +6,11 @@ to append a set of format_suffix_patterns in addition to the existing URLs.
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^$',views.SnippetList.as_view()),
-    url(r'^api$', views.api_root),
+    url(r'^$',views.SnippetList.as_view(), name='snippet-list'),
+    url(r'^api/$', views.api_root),
     url(r'^(?P<pk>[0-9]+)/$',views.SnippetDetail.as_view()),
     url(r'^(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
