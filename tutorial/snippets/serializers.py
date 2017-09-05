15264cf0cc734ac1315ec7c0bca5706de2b86015
toransahu <toran.sahu@yahoo.com>
2017-09-05 15:38:00 +0530

    added hyperlinks to Snippets & Users by implementing HyperlinkedModelSerializer class

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -3,42 +3,38 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 from django.contrib.auth.models import User
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     """
-    Creating new serializer class by inheriting ModelSerializer class.
-    This will help us avoid replicating all information from Snippet Model.
-    (Better than Serializer class)
-    *An automatically determined set of fields.
-    *Simple default implementations for the create() and update() methods.
+    1. Creating new serializer class by inheriting HyperlinkedModelSerializer class.
+    2. To use a hyperlinked style between entities.
+    (Better than ModelSerializer class)
+    3. The HyperlinkedModelSerializer has the following differences from ModelSerializer:
+        It does not include the id field by default.
+        It includes a url field, using HyperlinkedIdentityField.
+        Relationships use HyperlinkedRelatedField, instead of PrimaryKeyRelatedField.
     """
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+    ##  This field is of the same type as the url field,
+    ##  except that it points to the 'snippet-highlight' url pattern,
+    ##  instead of the 'snippet-detail' url pattern.
+    ##  go to url.py and add name='snippet-highlight'
     class Meta:
         model = Snippet
-        """
-        fields= (), tells about what fields are to be sent in response.
-        To send all the fields available in Model, use fields '__all__'.
-        """
-        fields =  ('id','title','code','linenos','language','style')
-        #fields = '__all__'
-        """
-        adding this to reflect association of Snippets & Users.
-        """
-        owner = serializers.ReadOnlyField(source='owner.username')
-
-        """
-        The field we've added is the untyped ReadOnlyField class,
-        in contrast to the other typed fields, such as CharField, BooleanField etc...
-        The untyped ReadOnlyField is always read-only, and will be used for serialized representations,
-        but will not be used for updating model instances when they are deserialized.
-        We could have also used CharField(read_only=True) here.
-        """
+        fields =  ('url', 'id', 'highlight', 'owner',
+                   'title','code','linenos','language','style')
+        ##  fields= (), tells about what fields are to be sent in response.
+        ##  To send all the fields available in Model, use fields = '__all__'.
+       
         
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-    """
-    'snippets' is a reverse relationship on the User model,
-    it will not be included by default when using the ModelSerializer class,
-    so we needed to add an explicit field for it.
-    """
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+    
+    ##  'snippets' is a reverse relationship on the User model,
+    ##  it will not be included by default when using the ModelSerializer or HyperlinkedModelSerializer class,
+    ##  so we needed to add an explicit field for it.
+    ##  Go to url.py and add name='snippet-detail'
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'id', 'username', 'snippets')
