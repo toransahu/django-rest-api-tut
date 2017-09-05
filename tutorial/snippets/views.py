827fa11a3eff927bfdecb34d1547115b7104967e
toransahu <toran.sahu@yahoo.com>
2017-09-05 17:07:41 +0530

    updated comment about @detail_route

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -25,12 +25,24 @@ class SnippetViewSet(viewsets.ModelViewSet):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
-
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)    
+    
     @detail_route(render_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
         return Response(snippet.highlighted)
+
+    
+    ##@detail_route decorator to create a custom action, named highlight.
+    ##This decorator can be used to add any custom endpoints
+    ##that don't fit into the standard create/update/delete style.
+    ##
+    ##Custom actions which use the @detail_route decorator will respond to GET requests by default.
+    ##We can use the methods argument if we wanted an action that responded to POST requests.
+    ##
+    ##The URLs for custom actions by default depend on the method name itself.
+    ##If you want to change the way url should be constructed,
+    ##you can include url_path as a decorator keyword argument.
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
