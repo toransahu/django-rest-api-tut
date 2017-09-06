736d3690182645a2f546d7f28a11afa29c04f304
toransahu <toran.sahu@yahoo.com>
2017-09-06 11:29:31 +0530

    added schema by implementing 'rest_framework.schemas'

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title = 'Tutorial Project')
 
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^app-quickstart/',include('quickstart.url')),
